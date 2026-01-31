11999b3f7e27b4f730d3df57e0db7252881e8f9e
Aymouni <pfpt.info.prot@gmail.com>
2026-01-31 11:08:33 -0800

    Class #1 Assignment

---
+++ assignment1.js
@@ -0,0 +1,31 @@
+// 1. Declare Variables
+
+// Using let
+let character = "Oliver";
+let place = "the subway station";
+let time = "early Monday morning";
+let object = "a singing backpack";
+let action = "dance with it";
+
+// Using const
+const situation = "a flash mob";
+
+// 2. Create the Story
+console.log("--- Funny Story ---");
+console.log(character + " was at " + place + " on " + time + ".");
+console.log("Suddenly, " + character + " saw " + object + " and decided to " + action + ".");
+console.log("It turned out to be " + situation + ", and everyone nearby was confused but happy!");
+
+// 3. Experiment with Different Values
+console.log("--- Different Version of the Story ---");
+
+// Change values to create a new story
+character = "Luna";
+place = "the airport";
+time = "a stormy evening";
+object = "a flying suitcase";
+action = "chase it across the terminal";
+
+console.log(character + " was at " + place + " on " + time + ".");
+console.log("Suddenly, " + character + " saw " + object + " and decided to " + action + ".");
+console.log("It turned out to be " + situation + ", and everyone had a great time!");
